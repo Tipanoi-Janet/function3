b8bd28d23642b7d9be99b4d0b66117decdc3610c
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-03 16:33:21 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main(){
+    division()
+
+}
+fun division () {
+
+    var num9 ="Janet"
+    println("Hello{name}")
+
+
+    var num1 = 240
+    var num2 = 45
+    println( num1 / num2)
+    println(num1 / num2 %2)
+
+
+    var num3 = 210
+    var num4 = 36
+    println(num1 + num2)
+
+    var num5 = 70
+    var num6 = 56
+    println(num5 + num6)
+
+
+    var num7 = 567
+    var num8 =345
+    println(num7 + num8)
+
+
+
+
+}
